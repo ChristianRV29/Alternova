ba52f1693b5e7326e7e7c477035b771d874d6671
Christian Riaño <christianrv.dev@gmail.com>
2022-12-24 09:29:57 -0500

    Create interfaces for new components

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,3 @@
-export type { Product, ThemeState } from './interfaces';
+export type { Product, ThemeState, QuantitySelectorProps } from './interfaces';
 export type { ThemeContextProps, ThemeAction } from './types';
 export { Theme } from './enums';
