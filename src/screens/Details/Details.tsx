1e4a18b69b0297810cbd73065a05b5dbe4c47391
Christian Riaño <christianrv.dev@gmail.com>
2022-12-26 07:58:47 -0500

    Move details styles into new file

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,13 +1,27 @@
 import React, { useContext, useState } from 'react';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import styled from '@emotion/native';
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { ProductsContext, ThemeContext } from '~src/context';
-import { CartIcon, HeroIcon, QuantitySelector, Toggler } from '~src/components';
+import { CartIcon, HeroIcon, QuantitySelector } from '~src/components';
 import { RootStackParamList } from '~src/@types';
+import {
+  AddToCartButton,
+  AddToCartText,
+  DetailsContainer,
+  HeroContainer,
+  ImageContainer,
+  LegoLogo,
+  ProductContainer,
+  ProductImage,
+  ProductPrice,
+  ProductStock,
+  ProductTitle,
+  Wrapper,
+} from './styles';
 
 type DetailsScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -75,106 +89,3 @@ export const Details = ({ navigation, route }: DetailsScreenProps) => {
     </Wrapper>
   );
 };
-
-export const Wrapper = styled.View<{ topSpacing: number }>`
-  background-color: ${({ theme }) => theme.colors.background};
-  display: flex;
-  flex-basis: 0;
-  flex-direction: column;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex: 1;
-  justify-content: flex-start;
-  padding: 10px;
-  top: ${({ topSpacing }) => `${topSpacing}px`};
-`;
-
-export const HeroContainer = styled.View`
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.card};
-  border-radius: 25px 0 25px 0;
-  border: ${({ theme }) => `3px solid ${theme.colors.border}`};
-  display: flex;
-  flex-direction: row;
-  height: 75px;
-  justify-content: space-between;
-  padding: 0 20px;
-  width: 100%;
-  margin-bottom: 20px;
-`;
-
-export const LegoLogo = styled.Image`
-  height: 50px;
-  width: 100px;
-`;
-
-export const ProductContainer = styled.View`
-  display: flex;
-  flex-basis: 0;
-  flex-direction: column;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex: 1;
-`;
-
-export const ImageContainer = styled.View`
-  align-items: center;
-  background-color: white;
-  display: flex;
-  height: 150px;
-  justify-content: center;
-  border-radius: 20px 20px 0 0;
-  width: 100%;
-`;
-
-export const DetailsContainer = styled.View`
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.notification};
-  border-radius: 0 0 20px 20px;
-  display: flex;
-  justify-content: space-between;
-  padding: 20px;
-  width: 100%;
-`;
-
-export const ProductTitle = styled.Text`
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 25px;
-  font-weight: bold;
-`;
-
-export const ProductImage = styled.Image`
-  height: 100px;
-  width: 100px;
-`;
-
-export const ProductStock = styled.Text`
-  color: ${({ theme }) => theme.colors.card};
-  font-size: 18px;
-  font-weight: 400;
-`;
-
-export const ProductPrice = styled.Text`
-  color: ${({ theme }) => theme.colors.card};
-  font-size: 20px;
-  font-weight: 700;
-`;
-
-export const AddToCartButton = styled.TouchableOpacity`
-  align-items: center;
-  background-color: white;
-  border-radius: 10px;
-  border: ${({ theme }) => theme.colors.primary};
-  display: flex;
-  flex-direction: row;
-  height: 30px;
-  justify-content: center;
-  width: 100%;
-`;
-
-export const AddToCartText = styled.Text`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 20px;
-  font-weight: 300;
-  margin-right: 5px;
-`;
