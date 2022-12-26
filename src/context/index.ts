b77149d5f492181d46bd01008451fee860aa5223
Christian Riaño <christianrv.dev@gmail.com>
2022-12-25 23:18:49 -0500

    Implement Firebase's auth service

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,2 +1,3 @@
 export { ProductsContext, ProductsProvider } from './Products';
 export { ThemeContext, ThemeProvider } from './Theme';
+export { AuthContext, AuthProvider } from './Auth';
--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,26 +1,36 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { RootStackParamList } from '~src/@types/types';
-import { Details, Home, Cart, Login } from '~src/screens';
+import { Details, Home, Cart, Login, Register } from '~src/screens';
 import { ThemeContext } from '~src/context/Theme';
+import { AuthContext } from '~src/context';
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export const StackNavigator = () => {
   const { theme } = useContext(ThemeContext);
+  const { status } = useContext(AuthContext);
   return (
     <NavigationContainer theme={theme}>
       <Navigator
         screenOptions={{
           headerShown: false,
         }}>
-        <Screen name="LoginScreen" component={Login} />
-        <Screen name="HomeScreen" component={Home} />
-        <Screen name="DetailsScreen" component={Details} />
-        <Screen name="CartScreen" component={Cart} />
+        {status !== 'authenticated' ? (
+          <Fragment>
+            <Screen name="LoginScreen" component={Login} />
+            <Screen name="RegisterScreen" component={Register} />
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Screen name="HomeScreen" component={Home} />
+            <Screen name="DetailsScreen" component={Details} />
+            <Screen name="CartScreen" component={Cart} />
+          </Fragment>
+        )}
       </Navigator>
     </NavigationContainer>
   );
